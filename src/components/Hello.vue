4f3522df3727a35b8669a9286cc0a12b9cc00097
Marcin Kalita <rubyconvict@gmail.com>
2017-07-10 10:47:45 +0200

    Add interactivity to Hello component
    C/P from: https://medium.com/codingthesmartway-com-blog/vue-js-2-quickstart-tutorial-2017-246195cfbdd2

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,31 +1,49 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+<div class="hello">
+  <h1>{{ msg }}</h1>
+  <hr />
+  <div>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li v-for="user in users">
+        {{user.firstname}} {{user.lastname}}
+      </li>
     </ul>
   </div>
+  <hr />
+  <div>
+    <input type="text" v-model="input_val">
+  </div>
+  <div>
+    Input Value: <span v-text="input_val"></span>
+  </div>
+  <hr />
+  <div>
+    <button class="btn btn-primary" v-on:click="counter++">You've clicked this button {{counter}} times!</button>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
   name: 'hello',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      users: [{
+          firstname: 'Sebastian',
+          lastname: 'Eschweiler'
+        },
+        {
+          firstname: 'Bill',
+          lastname: 'Smith'
+        },
+        {
+          firstname: 'John',
+          lastname: 'Porter'
+        }
+      ],
+      input_val: '',
+      counter: 0
     }
   }
 }
@@ -33,18 +51,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
 ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
+  list-style-position: inside;
 }
 
 a {
